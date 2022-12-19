ace27a526d12e5af879f8f1360b645549844e98d
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2022-12-19 11:28:33 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulEl = document.querySelector('#ingredients')
+const listIngradients = ingredients.map(ingredient => {
+	const liEl = document.createElement('li');
+liEl.textContent = `${ingredient}`;
+liEl.classList = 'item';
+ulEl.append(liEl);
+})
+
+
+
+	
+
