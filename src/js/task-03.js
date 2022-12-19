e869e1ba5a2cafcd6046417ccb37d9184153d038
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2022-12-19 18:12:19 +0200

    task-03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,29 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+//elem.insertAdjacentHTML(position, string);
+// "beforebegin" - перед elem
+// "afterbegin" - всередині elem, перед усіма дітьми
+// "beforeend" - всередині elem, після усіх дітей
+// "afterend" - після elem
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryImage = images.map(image => 
+	`<li class="gallery__item">
+	<img src = ${image.url} alt = ${image.alt}>
+	</li>`
+);
+galleryEl.insertAdjacentHTML('beforeend', galleryImage);
+console.log(galleryImage);
+
+// const galleryImage = images.map(image => {
+// 	const liElement = document.createElement('li');
+// liElement.classList = 'gallery__item';
+// const itemImage = document.createElement('img');
+// itemImage.src = image.url;
+// itemImage.alt = image.alt;
+// liElement.append(itemImage);
+// 	console.log(liElement);
+// 	galleryEl.append(liElement);
+// })
