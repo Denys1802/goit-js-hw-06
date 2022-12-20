06b951c9eb8b0b6b34a7707f7af60948a9180723
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2022-12-20 23:28:32 +0200

    task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+//console.log(getRandomHexColor());
+
+const btnEl = document.querySelector('.change-color');
+const textEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+//console.log(bodyEl.style);
+
+btnEl.addEventListener('click', changeBgColor);
+
+function changeBgColor() {
+	bodyEl.style.backgroundColor = getRandomHexColor();
+	textEl.textContent = getRandomHexColor();
+}
